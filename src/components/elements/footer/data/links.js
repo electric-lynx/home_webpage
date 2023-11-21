459ab83e30119a5fbe6586eae5ac93dd8aae0f4f
matterholt <matterholt.dev@gmail.com>
2023-11-21 07:38:41 -0500

    footer links

---
+++ src/components/elements/footer/data/links.js
@@ -0,0 +1,74 @@
+export const footerNav1 = [
+  {
+    itemText: "Marketing",
+    itemLink: "#",
+  },
+  {
+    itemText: "Analylitics",
+    itemLink: "#",
+  },
+  {
+    itemText: "Commerce",
+    itemLink: "#",
+  },
+  {
+    itemText: "Insights",
+    itemLink: "#",
+  },
+];
+
+export const footerSupport = [
+  {
+    itemText: "Pricing",
+    itemLink: "#",
+  },
+  {
+    itemText: "Guides",
+    itemLink: "#",
+  },
+  {
+    itemText: "FAQ",
+    itemLink: "#",
+  },
+  {
+    itemText: "Contact",
+    itemLink: "#",
+  },
+];
+
+export const footerCompany = [
+  {
+    itemText: "About",
+    itemLink: "/about",
+  },
+  {
+    itemText: "Contact",
+    itemLink: "#",
+  },
+  {
+    itemText: "Jobs",
+    itemLink: "#",
+  },
+  {
+    itemText: "Parteners",
+    itemLink: "#",
+  },
+  {
+    itemText: "Jobs",
+    itemLink: "#",
+  },
+];
+export const footerLegal = [
+  {
+    itemText: "Claim",
+    itemLink: "/#",
+  },
+  {
+    itemText: "Privacy",
+    itemLink: "#",
+  },
+  {
+    itemText: "Terms",
+    itemLink: "#",
+  },
+];
