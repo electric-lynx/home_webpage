b4ac0397983f5933b228a8fceb9bb85aa301b0aa
Joel Mounts <joel@JM.lan>
2023-11-25 21:33:52 -0500

    services section

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,38 +2,32 @@ const services = [
     {
         title: "Design",
         description: "The internet is filled with sub-par websites. We want to change that. Each of our websites is custom-made, fully-polished and built around your brand.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: 'puzzle'
     },
     {
         title: "SEO",
         description: "We design websites with SEO in mind, using modern web technologies that make your website easy for search engines to find and index.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: 'clipboard',
     },
     {
         title: "Mobile-Adaptive",
         description: "Over half of internet traffic is from mobile devices. We design with mobile-first principles while adapting to tablet- and desktop-sized screens.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: 'magnifyingglass',
     },
     {
         title: "Tracking",
         description: "What’s happening on your website? We provide Google Analytics by default, integrating smoothly with your Google Ad campaigns and tracking all site activity.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: 'mobilephone'
     },
     {
         title: "Security",
         description: "All sites come with HTTPS standard, ensuring your customers feel secure and are secure. All payment platforms feature state-of-the-art encryption.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: 'securityshield',
     },
     {
         title: "Customization",
         description: "We’re industry-professional coders. That means that we can handle custom projects, integrations, and scale your services as your business grows.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: 'transparentcube',
     },
 ];
 
