d58b9aff5ee0a6647571b64b1dcf13b5fb96b63c
matterholt <matterholt.dev@gmail.com>
2023-11-27 21:29:24 -0500

    Footer complete

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -2,4 +2,5 @@ import LinkSVG from "./LinkSVG.astro"
 import Container from "./Container.astro";
 import FooterNav from "./FooterNav.astro";
 import Paragraph from "./Paragraph.astro";
-export { LinkSVG, Container, FooterNav, Paragraph }
+import BtnLink from "./BtnLink.astro"
+export { LinkSVG, Container, FooterNav, Paragraph, BtnLink }
