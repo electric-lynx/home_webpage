b1bfbeb0b0e38caa891dfc02dff05814311f031f
matterholt <matterholt.dev@gmail.com>
2023-12-14 23:44:14 -0500

    working on pricing section

--- UI/src/components/sections/index.js
+++ UI/src/components/sections/index.js
@@ -1,10 +1,8 @@
 import Hero from "./main_hero/Hero.astro";
 import ContactUs from "./contact_us/ContactUs.astro";
-import LynxTeam from "./lynx_team/LynxTeam.astro"
-import ProvidedServices from "./provided_services/ProvidedServices.astro"
+import LynxTeam from "./lynx_team/LynxTeam.astro";
+import ProvidedServices from "./provided_services/ProvidedServices.astro";
 import CTA from "./call_to_action/CTA.astro";
+import CostOfServices from "./cost_of_services/CostOfServices.astro";
 
-
-export {
-    ContactUs, Hero, LynxTeam, ProvidedServices, CTA
-}
+export { ContactUs, Hero, LynxTeam, ProvidedServices, CTA, CostOfServices };
